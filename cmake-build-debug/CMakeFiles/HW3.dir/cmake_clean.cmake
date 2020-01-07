e51ea3994ea691f910814247eefc10fcffd4491d
tayaharshuk <57790114+tayaharshuk@users.noreply.github.com>
2020-01-07 21:05:25 +0200

    mm

---
+++ cmake-build-debug/CMakeFiles/HW3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HW3.dir/main.cpp.obj"
+  "HW3.exe"
+  "HW3.exe.manifest"
+  "HW3.pdb"
+  "libHW3.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HW3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
